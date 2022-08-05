0bfd5e5807a8de8212a839cd3de3ee433360efc3
Socratis <Seydinamouhamadousanoumbaye@esp.sn>
2022-08-05 01:11:00 +0000

    Add files via upload

--- app/src/main/java/com/example/AndroidProjectGLSIB/model/UserData.kt
+++ app/src/main/java/com/example/AndroidProjectGLSIB/model/UserData.kt
@@ -1,1 +1,14 @@
+package com.example.AndroidProjectGLSIB.model
 
+data class UserData (
+    var userName:String,
+    var userMb:String,
+    var userCat:String,
+    var userDesc:String,
+    var image : ByteArray
+        ){
+    var id: Int = -1
+    constructor(id:Int, userName: String, userMb: String, userCat: String, userDesc: String, image: ByteArray ) : this(userName, userMb, userCat, userDesc , image) {
+        this.id = id
+    }
+}
