fed6ab6c03de1d31fd6685a412b3f929fe8b0bf7
Socratis <Seydinamouhamadousanoumbaye@esp.sn>
2022-08-05 01:07:20 +0000

    Add files via upload

--- app/src/main/java/com/example/AndroidProjectGLSIB/MainApp.kt
+++ app/src/main/java/com/example/AndroidProjectGLSIB/MainApp.kt
@@ -1,1 +1,27 @@
+package com.example.AndroidProjectGLSIB
 
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.malkinfo.editingrecyclerview.R
+
+class MainApp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_app)
+
+        val getstarted = findViewById<Button>(R.id.getstarted)
+        val aboutUs = findViewById<Button>(R.id.aboutus)
+
+        getstarted.setOnClickListener{
+            val intentToHome: Intent = Intent(this, MainActivity::class.java)
+            startActivity(intentToHome)
+        }
+
+        aboutUs.setOnClickListener{
+            val intentAboutUs: Intent = Intent(this, AboutUs::class.java)
+            startActivity(intentAboutUs)
+        }
+    }
+}
